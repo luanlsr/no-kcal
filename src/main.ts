a81b543022fecb9a379aa8ead808e01d433ca392
luanlsr <luanfswd@gmail.com>
2024-02-27 02:28:57 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cors({
-    origin: '*',
+    origin: 'https://no-kcal-production.up.railway.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
